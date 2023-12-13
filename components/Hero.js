3d54c0103b90432e6c9a5bbeaba0ec3eb6548403
rohanwebuwu <rohanshah6982@gmail.com>
2023-12-13 13:18:09 +0530

    displays data

--- components/Hero.js
+++ components/Hero.js
@@ -6,12 +6,12 @@ function Hero() {
   return (
   
   
-  <><div className=" bg-gradient-to-tl from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent  h-[100vh]  text-7xl flex justify-center items-center text-center font-bold">Welcome to our project <br />
+  <><div className=" bg-gradient-to-tl from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent  h-[100vh]  md:text-7xl flex justify-center items-center text-center font-bold">Welcome to our project <br />
       Student Management System
     </div>
     
-    <div className=" flex justify-center">
-      To get started, click on the <User/> icon on the top right corner
+    <div className=" flex justify-center text-center">
+      To get started, click on the icon on the top right corner
 
 
       </div></>
--- components/Statusdata.js
+++ components/Statusdata.js
@@ -2,6 +2,13 @@
 import { db } from "@/firebaseConfig";
 import React, { use } from "react";
 import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
   getDocs,
   collection,
   getFirestore,
@@ -12,6 +19,7 @@ import {
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { Card } from "./ui/card";
 
 const Statusdata = () => {
   const [membershipdata, setmembershipdata] = useState([]);
@@ -21,17 +29,44 @@ const Statusdata = () => {
     });
   }, []);
 
-  console.log(membershipdata.map((doc) => doc.data()));
+  const dataa = membershipdata.map((doc) => doc.data());
+  console.log("dataa", dataa);
 
   const { data: session } = useSession();
 
   return (
-    <div>
-      new here?
-      <Link href="/senddata">send data</Link>
-      <br />
-      {session.user.name} <br />
-    </div>
+    <>
+      <div>
+        new here?
+        <Link href="/senddata">send data</Link>
+        <br />
+        {session.user.name} <br />
+      </div>
+      <div>
+        <div className=" grid gap-4 grid-cols-4 ">
+          {dataa.map((item) => (
+            <Card key={item.Sapid} className="">
+              <CardHeader>
+                <CardTitle>{item.name}</CardTitle>
+                <CardDescription>Student Data</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Sap Id:{item.Sapid}</p>
+              </CardContent>
+              <CardContent>
+                <p>Is Member:{item.membership}</p>
+              </CardContent>
+              <CardContent>
+                <p>Sap Id:{item.Sapid}</p>
+              </CardContent>
+              <CardFooter>
+    <p>{item.email}</p>
+  </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
