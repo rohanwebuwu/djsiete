8bc03de22acb4c4a2287b5b3bb02747b94a7489b
rohanwebuwu <rohanshah6982@gmail.com>
2024-02-14 19:56:44 +0530

    delete functionality

--- components/Statusdata.js
+++ components/Statusdata.js
@@ -1,7 +1,9 @@
 "use client";
 import { db } from "@/firebaseConfig";
-import React, { use } from "react";
+import { doc, getDoc, deleteDoc } from "firebase/firestore";
+
 import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
 import {
   CardContent,
   CardDescription,
@@ -15,7 +17,9 @@ import {
   getFirestore,
   onSnapshot,
   query,
+  Limit,
   orderBy,
+  where,
 } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
@@ -23,41 +27,48 @@ import Link from "next/link";
 import { Card } from "./ui/card";
 
 const Statusdata = () => {
+  const { data: session } = useSession();
   const [membershipdata, setmembershipdata] = useState([]);
+
+
+
+
   useEffect(() => {
     onSnapshot(query(collection(db, "membership")), (snapshot) => {
-      setmembershipdata(snapshot.docs);
+      setmembershipdata(snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})))
     });
+
   }, []);
 
-  const dataa = membershipdata.map((doc) => doc.data());
+  async function deleteData(id) {
+    deleteDoc(doc(db, "membership", id));
+    console.log("deleted");
+  }
 
-  const { data: session } = useSession();
+
+
 
   return (
     <>
       <div>
         new here?
         <Link href="/senddata">
-          
-          <Button variant="ghost">
-
-          send data
-          </Button>
-          </Link>
+          <Button variant="ghost">send data</Button>
+        </Link>
         <br />
         {session.user.name} <br />
       </div>
       <div>
         <div className=" grid gap-4 md:grid-cols-4 m-4 ">
-          {dataa.map((item) => (
-            <Card key={item.Sapid} className="">
+          {membershipdata.map((item) => (
+            <Card key={item.id} className="">
               <CardHeader>
                 <CardTitle>{item.name}</CardTitle>
                 <CardDescription>Student Data</CardDescription>
               </CardHeader>
               <CardContent>
                 <p>Sap Id:{item.Sapid}</p>
+
               </CardContent>
               <CardContent>
                 <p>Is Member:{item.membership}</p>
@@ -68,6 +79,17 @@ const Statusdata = () => {
               <CardFooter>
                 <p>{item.email}</p>
               </CardFooter>
+              <div className=" flex m-4 gap-6">
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    deleteData(item.id);
+                  }}
+                >
+                  Delete
+                </Button>
+                <Button variant="secondary">Edit</Button>
+              </div>
             </Card>
           ))}
         </div>
