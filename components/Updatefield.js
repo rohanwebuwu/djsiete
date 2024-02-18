ef896dff2331d07c12f00b2e70072f99943c40a2
rohanwebuwu <rohanshah6982@gmail.com>
2024-02-18 14:36:50 +0530

    final edit funcationality has been added

--- components/Updatefield.js
+++ components/Updatefield.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, setDoc,updateDoc } from "firebase/firestore"; 
 import { db } from "@/firebaseConfig";
 
 
 import { useState } from "react";
 function Updatefield(props) {
   const [sap, setsap] = useState("")
-  const [mem, setsmem] = useState("")
+  const [mem, setmem] = useState("")
 
   async function updateData() {
     await updateDoc(doc(db, "membership", props.id), {
-      sapid: "new sapid",
-      membership: "new membership status",
+      Sapid: sap,
+      membership: mem,
+
+
     });
+
+
   }
   return (
     <>
       <div className=" flex flex-col gap-4 m-9">
-        <Input type="text" placeholder="sapid" />
-        <Input type="text" placeholder="membership status" />
-        <Button>update data</Button>
+        <Input type="text" placeholder="sapid" onChange={(e)=>setsap(e.target.value)} />
+        <Input type="text" placeholder="membership status" onChange={(e)=>setmem(e.target.value)} />
+        <Button onClick={()=>updateData()}>Update Data</Button>
       </div>
     </>
   );
